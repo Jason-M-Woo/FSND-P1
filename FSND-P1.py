35f98c09e4f4c5a0b375c723796ad6a745b00d77
Jason Woo <jasonwoo@Jasons-iMac.local>
2018-08-06 15:37:31 -0400

    shebang and pep8

--- FSND-P1.py
+++ FSND-P1.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
 import psycopg2
 from datetime import datetime
 
 DBNAME = "news"
 
+
 def topThreeArticles():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select path, count(*) as views from log group by path order by views DESC limit 3 offset 1;")
+    c.execute('''select path, count(*) as views from log group by path
+                order by views DESC limit 3 offset 1;''')
     newTable = c.fetchall()
     print("MOST POPULAR 3 ARTICLES OF ALL TIME")
     for item in newTable:
@@ -16,10 +19,14 @@ def topThreeArticles():
 
 topThreeArticles()
 
+
 def bestAuthors():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select name, count(*) as views from (authors join articles on authors.id = articles.author) join log on articles.slug = substring(log.path, 10) group by name order by views DESC;")
+    c.execute('''select name, count(*) as views from
+                (authors join articles on authors.id = articles.author)
+                join log on articles.slug = substring(log.path, 10)
+                group by name order by views DESC;''')
     authors = c.fetchall()
     print("\nTOP AUTHORS OF ALL TIME")
     for item in authors:
@@ -28,10 +35,23 @@ def bestAuthors():
 
 bestAuthors()
 
+
 def findErrorDates():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select dates, percentage from (select finderror.dates, round(1.00*errors/clicks, 3) as percentage from (select substring(cast(time as varchar),1,10) as dates, count(*) as errors from log where status = '404 NOT FOUND' group by dates order by errors DESC) as finderror join (select substring(cast(time as varchar),1,10) as dates, count(*) as clicks from log group by dates order by clicks DESC) as findtotal on finderror.dates = findtotal.dates order by errors desc limit 3) as sortedtable where percentage > .01;")
+    c.execute('''select dates, percentage from
+                (select finderror.dates, round(1.00*errors/clicks, 3)
+                as percentage from
+                (select substring(cast(time as varchar),1,10)
+                as dates, count(*) as errors from log
+                where status = '404 NOT FOUND'
+                group by dates order by errors DESC) as finderror join
+                (select substring(cast(time as varchar),1,10)
+                as dates, count(*) as clicks from log group by dates
+                order by clicks DESC) as findtotal
+                on finderror.dates = findtotal.dates
+                order by errors desc limit 3)
+                as sortedtable where percentage > .01;''')
     days = c.fetchall()
     print("\nDAYS WITH OVER 1" + "%" + " ERRORS")
     for item in days:
